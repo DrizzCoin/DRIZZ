948185c690f48c4bbcde8044356a6a3b50ff2495
DRIZZ <DrizzC@proton.me>
2025-04-20 14:38:26 -0400

    Add LICENSE, SECURITY.md, CODE_OF_CONDUCT.md, and README.md

---
+++ components/drizzvia/TriviaGame.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const TriviaGame = () => {
+  return (
+    <div className="text-center">
+      <p className="text-lg font-medium">🧠 Trivia will begin shortly...</p>
+      {/* Timer + question components will go here */}
+    </div>
+  );
+};
