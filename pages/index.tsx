e9a398b25ebd7340cf74aef136828da8c8e2c64e
DRIZZ <DrizzC@proton.me>
2025-04-26 04:10:15 -0400

    Fix GitHub link alignment and layout in header

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home() {
               color: '#fff',
               display: 'flex',
               alignItems: 'center',
-              paddingTop: '0.25em' // slight adjustment to align visually with other links
+              paddingTop: '0.5em' // slight adjustment to align visually with other links
             }}
           >
             <img
